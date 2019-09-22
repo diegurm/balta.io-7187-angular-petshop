89e76b9eb04d437f92f0a0b514726e0fc79a1815
Diego Rodrigues <diegurm@hotmail.com>
2019-09-22 18:25:49 -0300

    Ajuste no style para validacao dos forms

--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../../services/data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
+  templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private readonly data: DataService,
+    private readonly fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
   }
-
 }
