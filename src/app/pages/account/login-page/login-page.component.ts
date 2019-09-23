5309d5017f9b7de0e330f704edd9f97388f28d9d
Diego Rodrigues <diegurm@hotmail.com>
2019-09-22 23:08:19 -0300

    Adicionado loading ao login, e diretiva de  mask e validação do cpf

--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -1,32 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../../services/data.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidator } from '../../../validators/custom.validator';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent implements OnInit {
-
   form: FormGroup;
+  public busy = false;
 
   constructor(
-    private readonly data: DataService,
+    private readonly service: DataService,
     private readonly fb: FormBuilder,
-  ) { }
+  ) {
+    this.form = this.fb.group({
+      username: [
+        '',
+        Validators.compose([
+          Validators.minLength(14),
+          Validators.maxLength(14),
+          Validators.required,
+          CustomValidator.isCpf()
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.minLength(6),
+          Validators.maxLength(20),
+          Validators.required,
+          CustomValidator.EmailValidator
+        ]),
+      ],
+    });
+  }
 
   ngOnInit() {
-    this.form = this.fb.group({
-      username: ['', Validators.compose([
-        Validators.minLength(11),
-        Validators.maxLength(11),
-        Validators.required
-      ])],
-      password: ['', Validators.compose([
-        Validators.minLength(6),
-        Validators.maxLength(20),
-        Validators.required
-      ])]
-    });
+    const token = localStorage.getItem('petshop.token');
+    if (token) {
+      this.busy = true;
+      this.service.refreshToken().subscribe(
+        (data: any) => {
+          localStorage.setItem('petshop.token', data.token);
+          this.busy = false;
+        },
+        err => {
+          console.log(err);
+          this.busy = false;
+        },
+      );
+    }
+  }
+
+  submit() {
+    this.busy = true;
+    this.service.authenticate(this.form.value).subscribe(
+      (data: any) => {
+        localStorage.setItem('petshop.token', data.token);
+        this.busy = false;
+      },
+      err => {
+        console.log(err);
+        this.busy = false;
+      },
+    );
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-
   public url = 'http://localhost:3000/v1';
 
-  constructor(
-    private readonly http: HttpClient,
-  ) {}
+  constructor(private readonly http: HttpClient) {}
+
+  private composeHeaders() {
+    const token = localStorage.getItem('petshop.token');
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+
+    return headers;
+  }
 
   getProducts() {
     return this.http.get<Product[]>(`${this.url}/products`);
@@ -19,5 +23,11 @@ export class DataService {
 
   authenticate(data) {
     return this.http.post(`${this.url}/accounts/authenticate`, data);
+  }
+
+  refreshToken() {
+    return this.http.post(`${this.url}/accounts/refresh-token`, null, {
+      headers: this.composeHeaders(),
+    });
   }
 }
