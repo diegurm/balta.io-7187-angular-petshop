26e1d7a70cfefc45e63ae864704f2e3a4d6938f3
Diego Rodrigues <diegurm@hotmail.com>
2019-11-21 00:50:26 -0300

    Update products-page.component.ts

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -3,14 +3,14 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { DataService } from '../../../services/data.service';
-import { Product } from '../../../models/product.model';
+import { ProductModel } from '../../../models/product.model';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent implements OnInit {
-  public $products: Observable<Product[]>;
+  public $products: Observable<ProductModel[]>;
 
   constructor(private readonly data: DataService) {}
 
